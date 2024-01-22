8c752b8014e186c41236700d3af999c33e52b90e
Cesar Caiafa <ccaiafa@cesars-mbp.ad18.riken.go.jp>
2024-01-22 17:34:40 +0900

    first skeleton for mwtomography physics model

---
+++ deepinv/physics/mwtomography.py
@@ -0,0 +1,55 @@
+import torch
+import numpy as np
+from torch import nn
+import torch.nn.functional as F
+from deepinv.physics.forward import Physics
+
+
+class MWTomography(Physics):
+    r"""
+    (MicroWave) Tomography nonlinear operator.
+
+    It computes the two steps:
+    (1) Calculate the total electric field Et such that Et = Ei + GD*LAMBDA*Et (linear problem);
+    (2) Calculate measurements Es = GS*LAMBDA*Et,
+    where GD and GS are the Green matrices and LAMBDA is a diagonal matrix containing in it main diagonal the vectorized complex object image.
+
+    :param int img_width: width/height of the square image input.
+    :param float wavelength: working wavelength
+    :param int no_of_receivers: number of receiver antennas
+    :param int no_of_transmitters: number of transmitter antennas
+    :param float max_diameter: maximum object diameter
+    :param float min_permittivity: minimum object permittivity
+    :param float max_permittivity: maximum object permittivity
+    :param float receiver_radius: receiver distance to center
+    :param float transmitter_radius: transmitter distance to center
+    :param bool wave_type: 0=linear, 1:planar
+    :param str shape: "circle", "rectangle"
+    :param int nshapes: number of shapes in the image
+    :param str device: gpu or cpu.
+    """
+
+    def __init__(
+        self,
+        img_width,
+        wavelength,
+        no_of_receivers,
+        no_of_transmitters,
+        max_diameter=1,
+        min_permittivity=1.0001,
+        max_permittivity=1.5,
+        receiver_radius=3,
+        transmitter_radius=3,
+        wave_type=0,
+        shape="circle",
+        nshapes=3,
+        device=torch.device("cpu"),
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+
+    def A(self, x):
+        return
+
+    def A_dagger(self, y):
+        return
