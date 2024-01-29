0723f905cbaba349de62c8aba9b4ca5c5dbe10de
Cesar Caiafa <ccaiafa@cesars-mbp.ad18.riken.go.jp>
2024-01-29 17:54:21 +0900

    remove ET as parameter in linear operator

--- deepinv/physics/mwtom.py
+++ deepinv/physics/mwtom.py
@@ -33,51 +33,50 @@ class MWTomography(Physics):
     """
 
     def __init__(
-        self,
-        img_width,
-        wavelength,
-        no_of_receivers,
-        no_of_transmitters,
-        max_diameter=1,
-        min_permittivity=1.0001,
-        max_permittivity=1.5,
-        receiver_radius=3,
-        transmitter_radius=3,
-        wave_type=0,
-        shape="circle",
-        nshapes=3, device=torch.device("cpu"), **kwargs,):
-
+            self,
+            img_width,
+            wavelength,
+            no_of_receivers,
+            no_of_transmitters,
+            max_diameter=1,
+            min_permittivity=1.0001,
+            max_permittivity=1.5,
+            receiver_radius=3,
+            transmitter_radius=3,
+            wave_type=0,
+            shape="circle",
+            nshapes=3, device=torch.device("cpu"), **kwargs, ):
         super().__init__(**kwargs)
 
-
     def A(self, x):
-
         return
-    
+
     def A_dagger(self, y):
         print()
         return
 
+
 class stepBOp(LinearOperator):
-    def __init__(self, GS, dtype=None):
+    def __init__(self, GS):
         self.GS = GS
         super().__init__()
-        
-    def _matvec(self, x, ET):
+
+    def _matvec(self, x):
+        _, ET = generate_total_electric_field(self, x, self.x_domain, self.y_domain, full_pixel=True)
         # forward linear operator
         aux = np.multiply(np.matrix(ET), np.matrix(x).T)
         y = np.array(np.matmul(-1j * self.GS, aux))
         return y.reshape(-1)
-    
-    def _rmatvec(self, y, ET):
+
+    def _rmatvec(self, y):
+        _, ET = generate_total_electric_field(self, x, self.x_domain, self.y_domain, full_pixel=True)
         # adjoint linear operator
         B = np.matmul(1j * self.GS.H, y.reshape(-1, self.Ninc))
         C = np.multiply(np.matrix(ET).H, B.T)
         C = np.sum(C, axis=0)
         C = np.matrix(C).T
         return C.reshape(-1)
-        
-        
+
 
 class MWTstepB(Physics):
     r"""
@@ -104,17 +103,17 @@ class MWTstepB(Physics):
     """
 
     def __init__(
-        self,
-        img_width,
-        wavelength,
-        no_of_receivers,
-        no_of_transmitters,
-        max_diameter=1,
-        receiver_radius=3,
-        transmitter_radius=3,
-        wave_type=0,
-        device=torch.device("cpu"),
-        **kwargs
+            self,
+            img_width,
+            wavelength,
+            no_of_receivers,
+            no_of_transmitters,
+            max_diameter=1,
+            receiver_radius=3,
+            transmitter_radius=3,
+            wave_type=0,
+            device=torch.device("cpu"),
+            **kwargs
     ):
         super().__init__(**kwargs)
         self.electric_field_generator = ElectricFieldGenerator(
@@ -125,18 +124,19 @@ class MWTstepB(Physics):
             wavelength=wavelength,
             receiver_radius=receiver_radius,
             transmitter_radius=transmitter_radius,
-            wave_type=wave_type)
+            wave_type=wave_type
+            )
 
         image_domain = np.linspace(-max_diameter, max_diameter, img_width)
         self.x_domain, self.y_domain = np.meshgrid(image_domain, -image_domain)
         self.GS = self.electric_field_generator.compute_GS(self.x_domain, self.y_domain)
-        
-        self.stepBLinOp = stepBOp(self.GS, device)
+
+        self.stepBLinOp = stepBOp(self.GS)
 
     def A(self, x):
-        return self.stepBLinOp._matvec(x, ET)
+        return self.stepBLinOp._matvec(x)
 
     def A_adjoint(self, y):
-        return self.stepBLinOp._rmatvec(y, ET)
-        
+        return self.stepBLinOp._rmatvec(y)
+
         return
